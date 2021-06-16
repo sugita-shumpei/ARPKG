940ffa3b1d2e22edc53cb8d19f5917aeeba87e49
kTadaR <kazuki.tada@fj.ics.keio.ac.jp>
2021-06-16 18:30:44 +0900

    Fixed DrawBounds.

--- Assets/Scripts/DrawBounds.cs
+++ Assets/Scripts/DrawBounds.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class DrawBounds : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject bounds;
+    private GameObject baseObject;
     private GameObject newBounds;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        newBounds = Instantiate(bounds, transform.position, Quaternion.identity) as GameObject;
+        baseObject = gameObject;
+        newBounds = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        var boundsCollider = newBounds.GetComponent<Collider>();
+        Destroy(boundsCollider);
+        
+        newBounds.transform.parent = baseObject.transform;
+        newBounds.transform.localScale = baseObject.GetComponent<Collider>().bounds.size;
+        newBounds.transform.localScale = new Vector3(
+            newBounds.transform.localScale.x / baseObject.transform.localScale.x,
+            newBounds.transform.localScale.y / baseObject.transform.localScale.y,
+            newBounds.transform.localScale.z / baseObject.transform.localScale.z
+            );
+
+        newBounds.transform.position = baseObject.GetComponent<Collider>().bounds.center;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        var col = GetComponent<Collider>();
-        if ( col == null)
-        {
-            newBounds.SetActive(false);
-        } else
-        {
-            newBounds.SetActive(true);
-            newBounds.transform.localScale = col.bounds.size;
-        }
+
     }
 }
