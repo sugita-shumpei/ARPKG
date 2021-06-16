83690328a4836171f377328ae57a7d61503393fb
shumpei.sugita <sugita.shumpei@fj.ics.keio.ac.jp>
2021-06-16 17:28:36 +0900

    add MovableBox
    add MovableBox

---
+++ Assets/Scripts/MovableBox.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableBox : MonoBehaviour
+{
+
+    private Vector3 moveTo;
+
+    private bool beRay = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RayCheck();
+        }
+
+        if (beRay)
+        {
+            MovePoisition();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            beRay = false;
+        }
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == gameObject.GetComponent<Collider>())
+        {
+            beRay = true;
+        }
+        else
+        {
+            beRay = false;
+        }
+
+    }
+
+    private void MovePoisition()
+    {
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+
+        moveTo = GetComponent<Camera>().ScreenToWorldPoint(mousePos);
+        transform.position = moveTo;
+
+    }
+}
