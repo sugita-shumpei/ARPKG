4d8711457ca8ec8af409e74a92f293e9d8c2d8bd
shumpei.sugita <sugita.shumpei@fj.ics.keio.ac.jp>
2021-06-23 15:17:30 +0900

    fix
    fix

--- Assets/Scripts/BallGenerator.cs
+++ Assets/Scripts/BallGenerator.cs
@@ -20,25 +20,25 @@ public class BallGenerator : MonoBehaviour
     private Vector3       _maxRange;
     void Start()
     {
-        _parent     = transform.root.gameObject;
-        _genObjects = new GameObject [_numObjects];
-        Debug.Assert(_baseObject!=null);
+        _genObjects = new GameObject[_numObjects];
         for(int i=0;i<_numObjects;++i){
             Vector3 randomPosition;
-            randomPosition.x = Random.Range(_minRange.x,_maxRange.x);
-            randomPosition.y = Random.Range(_minRange.x,_maxRange.y);
-            randomPosition.z = Random.Range(_minRange.x,_maxRange.z);
-            _genObjects[i]   = GameObject.Instantiate(_baseObject) as GameObject;
+            randomPosition.x                       = Random.Range(_minRange.x,_maxRange.x);
+            randomPosition.y                       = Random.Range(_minRange.x,_maxRange.y);
+            randomPosition.z                       = Random.Range(_minRange.x,_maxRange.z);
+            _genObjects[i]                         = GameObject.Instantiate(_baseObject) as GameObject;
             _genObjects[i].transform.parent        = _baseObject.transform.parent;
             _genObjects[i].transform.localPosition = randomPosition;
             _genObjects[i].transform.localScale    = _localScale;
             Debug.Log(randomPosition);
         }
+        _parent                                    = transform.root.gameObject;
+        Debug.Assert(_baseObject!=null);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
