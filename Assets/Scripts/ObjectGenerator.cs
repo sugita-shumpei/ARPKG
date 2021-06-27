38b372e87b1b13f9d519cbb2f6d6a68cd685f873
kTadaR <kazuki.tada@fj.ics.keio.ac.jp>
2021-06-27 19:04:19 +0900

    Added Object Generator related to add Object GUI.

---
+++ Assets/Scripts/ObjectGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform _parentObject;
+    [SerializeField] private GameObject[] _prefabs;
+    [SerializeField] private Button[] _buttons;
+    public void Start()
+    {
+        for(int i = 0; i < _prefabs.Length; i++)
+        {
+            _buttons[i].GetComponent<Button>().onClick.AddListener(() => { GenerateObject(_prefabs[i]); });
+        }
+
+    }
+
+    private void GenerateObject(GameObject gb)
+    {
+        GameObject obj = Instantiate(gb);
+        obj.transform.parent = _parentObject;
+        obj.transform.localPosition = new Vector3(0, 0, 0);
+        obj.GetComponent<Rigidbody>().useGravity = false;
+
+    }
+}
