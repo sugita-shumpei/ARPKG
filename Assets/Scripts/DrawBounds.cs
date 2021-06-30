17d126ef49ad6ed0204308e80b012ec4b05eae53
kTadaR <kazuki.tada@fj.ics.keio.ac.jp>
2021-06-30 17:27:34 +0900

    Deleting ExecuteInEditMode attribute in DrawBounds.

--- Assets/Scripts/DrawBounds.cs
+++ Assets/Scripts/DrawBounds.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class DrawBounds : MonoBehaviour
 {
     private GameObject baseObject;
